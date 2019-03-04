a2a8ef21f2076cab2179333b172dacac22dc4ac6
uncleYO18 <dr.agoy007@gmail.com>
2019-03-04 17:57:15 +0700

    mencoba layout ListView

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Mencoba Flutter",
-      home: Kartu(),
-      // home: DaftarView(),
+      // home: Kartu(),
+      home: DaftarView(),
       // home: LihatGrid(),
     );
 
